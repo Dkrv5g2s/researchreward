69d4f15a8271179bd0a8d5c581d7a332efe91d21
游上逸 <landy6080860908@gmail.com>
2020-12-09 16:14:59 +0800

    建立初始servlet

--- src/main/java/servlet/ExcellentResearcher/ResearchProductionServlet.java
+++ src/main/java/servlet/ExcellentResearcher/ResearchProductionServlet.java
@@ -1,4 +1,4 @@
-package servlet;
+package servlet.ExcellentResearcher;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -6,20 +6,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static Utils.DocxToPdfUtil.wordToPDF;
 
-public class TestServlet extends HttpServlet {
+public class ResearchProductionServlet extends HttpServlet {
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //wordToPDF("C:\\Users\\z\\Desktop\\ResearchRewardSystem\\src\\doc\\1.docx","C:\\Users\\z\\Desktop\\ResearchRewardSystem\\src\\doc\\1.pdf");
+
         req.getRequestDispatcher("WEB-INF/jsp/ExcellentResearcher/ResearchProduction.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //wordToPDF("C:\\Users\\z\\Desktop\\ResearchRewardSystem\\src\\doc\\1.docx","C:\\Users\\z\\Desktop\\ResearchRewardSystem\\src\\doc\\1.pdf");
+
         req.getRequestDispatcher("WEB-INF/jsp/ExcellentResearcher/ResearchProduction.jsp").forward(req, resp);
     }
 }
