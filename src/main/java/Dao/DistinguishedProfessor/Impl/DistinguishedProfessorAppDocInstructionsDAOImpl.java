97835971a9940c8d5d12f2829d89f390e5b8b74f
108598042 <orz123451@gmail.com>
2021-01-16 12:17:16 +0800

    特聘教授審查資料說明

---
+++ src/main/java/Dao/DistinguishedProfessor/Impl/DistinguishedProfessorAppDocInstructionsDAOImpl.java
@@ -0,0 +1,97 @@
+package Dao.DistinguishedProfessor.Impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Bean.DistinguishedProfessor.DistinguishedProfessorAppDocInstructions;
+import DBConnection.DBConnection;
+import DBConnection.DBConnectionImpl;
+import Dao.DistinguishedProfessor.DistinguishedProfessorAppDocInstructionsDAO;
+
+public class DistinguishedProfessorAppDocInstructionsDAOImpl implements DistinguishedProfessorAppDocInstructionsDAO{
+	private DBConnection dbConnection = new DBConnectionImpl();
+    private static final String SELECT = "SELECT * FROM distinguishedprofessorappdocinstructions WHERE userNumber = ?"; 
+    private static final String INSERT = 
+    		"INSERT INTO distinguishedprofessorappdocinstructions (userNumber,technologyTransferContractName,technologyTransferDepartment,contractDate,technologyTransferFund,technologyTransferFundBringInDate)"+
+    		" values(?,?,?,?,?,?)";
+    private static final String DELETE = 
+    		"DELETE FROM distinguishedprofessorappdocinstructions "+
+    		" WHERE userNumber = ?";
+    @Override
+    public void save(List<DistinguishedProfessorAppDocInstructions> object, String userNumber) {
+        Connection connection = dbConnection.getConnection();
+        
+        delete(connection,userNumber);
+        if(!object.isEmpty()) {
+            insert(connection,object);
+        }
+        	
+    }
+
+    @Override
+    public List<DistinguishedProfessorAppDocInstructions> show(String userNumber) {
+        Connection connection = dbConnection.getConnection();
+        ResultSet resultSet = null;
+        List<DistinguishedProfessorAppDocInstructions> list = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT))
+        {
+        	preparedStatement.setString(1,userNumber);
+        	
+        	resultSet = preparedStatement.executeQuery();
+        	while(resultSet.next()) {
+        		DistinguishedProfessorAppDocInstructions dpadi = null;
+        		dpadi = new DistinguishedProfessorAppDocInstructions(
+        				resultSet.getString("userNumber"),
+        				resultSet.getString("technologyTransferContractName"),
+        				resultSet.getString("technologyTransferDepartment"),
+        				resultSet.getString("contractDate"),
+        				resultSet.getString("technologyTransferFund"),
+        				resultSet.getString("technologyTransferFundBringInDate"));
+        		list.add(dpadi);
+        	}
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            
+        }
+        return list;
+    }
+    
+    public void insert(Connection connection,List<DistinguishedProfessorAppDocInstructions> object) {
+    	try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT))
+        {
+            for(int i = 0;i<object.size();i++) {
+            	preparedStatement.setString(1,object.get(i).getUserNumber());
+                preparedStatement.setString(2,object.get(i).getTechnologyTransferContractName());
+                preparedStatement.setString(3,object.get(i).getTechnologyTransferDepartment());
+                preparedStatement.setString(4,object.get(i).getContractDate());
+                preparedStatement.setString(5,object.get(i).getTechnologyTransferFund());
+                preparedStatement.setString(6,object.get(i).getTechnologyTransferFundBringInDate());
+                
+                preparedStatement.addBatch();
+            }		
+            preparedStatement.executeBatch();            
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void delete(Connection connection,String userNumber) {
+    	try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE))
+        {
+    
+    		preparedStatement.setString(1,userNumber);
+ 		
+    		preparedStatement.executeUpdate();
+    		
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
