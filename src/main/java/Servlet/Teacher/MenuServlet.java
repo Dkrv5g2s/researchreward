4da53e5d6959b105d10ec126c04e16a6af565ca6
游上逸 <landy6080860908@gmail.com>
2021-01-21 17:46:21 +0800

    modify

--- src/main/java/Servlet/Teacher/MenuServlet.java
+++ src/main/java/Servlet/Teacher/MenuServlet.java
@@ -1,12 +1,14 @@
 package Servlet.Teacher;
 
+import Servlet.login.ServletEntryPoint;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MenuServlet extends HttpServlet {
+public class MenuServlet extends ServletEntryPoint {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
