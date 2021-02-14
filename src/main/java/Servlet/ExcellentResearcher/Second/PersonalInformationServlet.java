cea71724baafcadd831a9799509a1daa3636775c
游上逸 <landy6080860908@gmail.com>
2021-02-14 15:00:31 +0800

    續撥獎勵 個人資料 完成

---
+++ src/main/java/Servlet/ExcellentResearcher/Second/PersonalInformationServlet.java
@@ -0,0 +1,43 @@
+package Servlet.ExcellentResearcher.First;
+
+
+import Bean.ExcellentResearcher.PersonalInformation.PersonalInformation;
+import Service.ExcellentResearcher.PersonalInformationService;
+import Servlet.login.ServletEntryPoint;
+import fr.opensagres.xdocreport.document.json.JSONObject;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+public class PersonalInformationServlet extends ServletEntryPoint {
+
+    private PersonalInformationService personalInformationService= new PersonalInformationService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        req.setCharacterEncoding("UTF-8");
+        req.setAttribute("json",personalInformationService.get(Integer.valueOf((String)session.getAttribute("projectId"))));
+
+        req.getRequestDispatcher("WEB-INF/jsp/ExcellentResearcher/First/personalinformation.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+
+        JSONObject json = new JSONObject(readJSONString(req));
+        personalInformationService.save(json,Integer.valueOf((String)session.getAttribute("projectId")));
+
+        req.getRequestDispatcher("WEB-INF/jsp/ExcellentResearcher/First/RecruitDescription.jsp").forward(req, resp);
+    }
+
+
+}
