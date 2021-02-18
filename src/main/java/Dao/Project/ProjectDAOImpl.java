b52f24e2ddef74883c3cab7881242034b277a452
109598037 <t109598037@ntut.org.tw>
2021-02-18 15:03:51 +0800

    送審流程 未完成

--- src/main/java/Dao/Project/ProjectDAOImpl.java
+++ src/main/java/Dao/Project/ProjectDAOImpl.java
@@ -15,6 +15,7 @@ public class ProjectDAOImpl implements ProjectDAO {
     private DBConnection dbConnection = new DBConnectionImpl();
     private static final String INSERT_PROJECT = "INSERT INTO reward_project (staff_code,status,reward_type) values(?,?,?)";
     private static final String GET_PROJECT = "SELECT * FROM reward_project WHERE staff_code=? AND status=?";
+    private static final String ADMINS_GET_PROJECT = "SELECT * FROM reward_project WHERE status=?";
     private static final String UPDATE_PROJECT_STATUS = "UPDATE reward_project SET status=? WHERE project_id =?";
     private static final String DELETE_PROJECT = "DELETE FROM reward_project WHERE project_id = ? AND staff_code=?";
     private static final String GET_REWARD_TYPE = "SELECT reward_type FROM reward_project WHERE project_id=?";
@@ -51,14 +52,13 @@ public class ProjectDAOImpl implements ProjectDAO {
     }
 
     @Override
-    public void UpdateProjectStatus(RewardProject rewardProject) {
+    public void UpdateProjectStatus(int project_id,String status) {
         Connection connection = dbConnection.getConnection();
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_PROJECT_STATUS))
         {
-            preparedStatement.setString(1,rewardProject.getStatus());
-            preparedStatement.setInt(2,rewardProject.getProject_id());
-            preparedStatement.setString(3,rewardProject.getStaff_code());
+            preparedStatement.setString(1, status);
+            preparedStatement.setInt(2, project_id);
             preparedStatement.executeUpdate();
             connection.close();
         } catch (SQLException e) {
@@ -93,7 +93,31 @@ public class ProjectDAOImpl implements ProjectDAO {
         return result;
     }
 
+    @Override
+    public List<RewardProject> adminsGetProjects(String status) {
+        Connection connection = dbConnection.getConnection();
+        List<RewardProject> result = new ArrayList<>();
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement(ADMINS_GET_PROJECT))
+        {
+            preparedStatement.setString(1,status);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                while (resultSet.next()){
+                    result.add(new RewardProject(resultSet.getInt("project_id"),
+                            resultSet.getString("staff_code"),
+                            resultSet.getString("status"),
+                            resultSet.getString("reward_type")));
+                }
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 
     public String getRewardType(int project_id) {
         Connection connection = dbConnection.getConnection();
---
+++ src/main/java/Servlet/admin/RewardPendingListServlet.java
@@ -0,0 +1,49 @@
+package Servlet.admin;
+
+import Service.Admin.RewardPendingListService;
+import Service.Teacher.RewardListService;
+import Servlet.login.ServletEntryPoint;
+import fr.opensagres.xdocreport.document.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class RewardPendingListServlet extends ServletEntryPoint {
+
+    private RewardPendingListService service = new RewardPendingListService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+        String userNumber = (String) session.getAttribute("userNumber");
+        req.setAttribute("json",service.getPendingList(userNumber));
+
+        req.getRequestDispatcher("WEB-INF/jsp/admin/rewardPendingList.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        JSONObject json = new JSONObject(readJSONString(req));
+
+        HttpSession session = req.getSession();
+        session.setAttribute("projectId",json.getString("projectId"));
+
+        resp.sendRedirect(service.getCatalogURL(json.getString("rewardName")));
+    }
+
+//    @Override
+//    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        req.setCharacterEncoding("utf-8");
+//        JSONObject json = new JSONObject(readJSONString(req));
+//
+//        HttpSession session = req.getSession();
+//        session.setAttribute("projectId",json.getString("projectId"));
+//
+//        resp.sendRedirect(service.del(json.getString("rewardName")));
+//    }
+}
