ef16971438a4f10f57e3bc7aec5533fd289c417b
109598037 <t109598037@ntut.org.tw>
2021-02-19 18:20:43 +0800

    將SQL Table reward_project 中 staus 抽出成另一個Table, 以及微調送出申請動作

--- src/main/java/Dao/Project/ProjectDAOImpl.java
+++ src/main/java/Dao/Project/ProjectDAOImpl.java
@@ -13,21 +13,23 @@ import java.util.List;
 
 public class ProjectDAOImpl implements ProjectDAO {
     private DBConnection dbConnection = new DBConnectionImpl();
-    private static final String INSERT_PROJECT = "INSERT INTO reward_project (staff_code,status,reward_type) values(?,?,?)";
-    private static final String GET_PROJECT = "SELECT * FROM reward_project WHERE staff_code=? AND status=?";
-    private static final String ADMINS_GET_PROJECT = "SELECT * FROM reward_project WHERE status=?";
-    private static final String UPDATE_PROJECT_STATUS = "UPDATE reward_project SET status=? WHERE project_id =?";
+    private static final String INSERT_PROJECT = "INSERT INTO reward_project (staff_code, status_id, reward_type) values(?,?,?)";
+    private static final String GET_PERSONAL_PROJECTS = "SELECT * FROM reward_project LEFT JOIN reward_project_status on reward_project.status_id=reward_project_status.status_id WHERE staff_code=? AND reward_project.status_id BETWEEN ? AND ? ";
+    private static final String GET_STATUS_ID_BY_PROJECT_ID = "SELECT status_id FROM reward_project WHERE project_id =?";
+    private static final String ADMINS_GET_PENDING_PROJECTS = "SELECT * FROM reward_project LEFT JOIN reward_project_status on reward_project.status_id=reward_project_status.status_id WHERE reward_project.status_id=?";
+    private static final String UPDATE_PROJECT_STATUS = "UPDATE reward_project SET status_id=? WHERE project_id =?";
     private static final String DELETE_PROJECT = "DELETE FROM reward_project WHERE project_id = ? AND staff_code=?";
     private static final String GET_REWARD_TYPE = "SELECT reward_type FROM reward_project WHERE project_id=?";
+    private static final String GET_MAX_STATUS_ID = "SELECT status_id FROM rss.reward_project_status order by status_id DESC LIMIT 1";
 
     @Override
-    public void insertNewProject( String staff_code, String status, String reward_type ) {
+    public void insertNewProject( String staff_code, int status_id, String reward_type ) {
         Connection connection = dbConnection.getConnection();
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PROJECT))
         {
             preparedStatement.setString(1,staff_code);
-            preparedStatement.setString(2,status);
+            preparedStatement.setInt(2,status_id);
             preparedStatement.setString(3,reward_type);
             preparedStatement.executeUpdate();
             connection.close();
@@ -52,12 +54,12 @@ public class ProjectDAOImpl implements ProjectDAO {
     }
 
     @Override
-    public void UpdateProjectStatus(int project_id,String status) {
+    public void updateProjectStatus(int project_id, int status_id) {
         Connection connection = dbConnection.getConnection();
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_PROJECT_STATUS))
         {
-            preparedStatement.setString(1, status);
+            preparedStatement.setInt(1, status_id);
             preparedStatement.setInt(2, project_id);
             preparedStatement.executeUpdate();
             connection.close();
@@ -67,14 +69,15 @@ public class ProjectDAOImpl implements ProjectDAO {
     }
 
     @Override
-    public List<RewardProject> getProjects(String staffCode, String status) {
+    public List<RewardProject> getPersonalProjects(String staffCode, int begin_status_id, int end_status_id) {
         Connection connection = dbConnection.getConnection();
         List<RewardProject> result = new ArrayList<>();
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(GET_PROJECT))
+        try (PreparedStatement preparedStatement = connection.prepareStatement(GET_PERSONAL_PROJECTS))
         {
             preparedStatement.setString(1,staffCode);
-            preparedStatement.setString(2,status);
+            preparedStatement.setInt(2,begin_status_id);
+            preparedStatement.setInt(3,end_status_id);
 
             try (ResultSet resultSet = preparedStatement.executeQuery()){
                 while (resultSet.next()){
@@ -94,13 +97,32 @@ public class ProjectDAOImpl implements ProjectDAO {
     }
 
     @Override
-    public List<RewardProject> adminsGetProjects(String status) {
+    public int getStatusIdByProjectId(int project_id) {
+        Connection connection = dbConnection.getConnection();
+        int result = 0;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(GET_STATUS_ID_BY_PROJECT_ID))
+        {
+            preparedStatement.setInt(1,project_id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                if (resultSet.next()){
+                    result = resultSet.getInt("status_id");
+                }
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public List<RewardProject> adminsGetPendingProjects(int status_id) {
         Connection connection = dbConnection.getConnection();
         List<RewardProject> result = new ArrayList<>();
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(ADMINS_GET_PROJECT))
+        try (PreparedStatement preparedStatement = connection.prepareStatement(ADMINS_GET_PENDING_PROJECTS))
         {
-            preparedStatement.setString(1,status);
+            preparedStatement.setInt(1,status_id);
 
             try (ResultSet resultSet = preparedStatement.executeQuery()){
                 while (resultSet.next()){
@@ -129,6 +151,24 @@ public class ProjectDAOImpl implements ProjectDAO {
                 if (resultSet.next()){
                     result = resultSet.getString("reward_type");
                     System.out.println("result:"+result);
+                }
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public int getMaxStatusId() {
+        Connection connection = dbConnection.getConnection();
+        int result = 0;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(GET_MAX_STATUS_ID))
+        {
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                if (resultSet.next()){
+                    result = resultSet.getInt("status_id");
                 }
             }
             connection.close();
