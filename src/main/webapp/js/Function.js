f58da61da122f79e769b2744d2d6101440ea47b5
游上逸 <landy6080860908@gmail.com>
2021-02-22 11:20:50 +0800

    按鈕disabled -> remove

--- src/main/webapp/js/Function.js
+++ src/main/webapp/js/Function.js
@@ -2,7 +2,7 @@ function setReadOnly(flags){
     if (flags == true){
         $("input").prop("readonly",true);
         $("textarea").prop("readonly",true);
-        $("button").prop("disabled",true);
+        $("button").remove();
     }else{
         //don't need set readonly
     }
