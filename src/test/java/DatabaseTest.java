a3ae53a606ca14d9e3092635dd0b5e16020a358a
TedLin <t109598073@ntut.org.tw>
2021-03-01 16:04:06 +0800

    [feature] 新增教評DB連線Test 加入使用提醒

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -12,15 +12,18 @@ public class DatabaseTest {
     public void DatabaseConnectionTest(){
         DBConnectionImpl db = new DBConnectionImpl();
         Connection connection = db.getConnection();
-        System.out.println(connection);
+//        System.out.println(connection);
         Assert.assertNotNull(connection);
 
     }
     @Test
     public void TeacherEvaluationDBConnectionTest(){
+
+        // 專案目錄不可以有中文字 否則會失敗!
+        // ref : https://blog.csdn.net/weixin_43584574/article/details/102657174
         TeacherEvaluationDBConnectionImpl TEdb = new TeacherEvaluationDBConnectionImpl();
         Connection connection = TEdb.getConnection();
-        System.out.println(connection);
+//        System.out.println(connection);
         Assert.assertNotNull(connection);
 
     }
