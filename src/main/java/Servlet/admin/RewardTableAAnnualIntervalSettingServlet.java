cd3fd3dc0fd467ba2be5effe223df2da2f7e206d
TedLin <t109598073@ntut.org.tw>
2021-08-11 09:32:07 +0800

    [feature]研發處管理端 加入 各獎項期刊論文統計申請表年度設定

---
+++ src/main/java/Servlet/admin/RewardTableAAnnualIntervalSettingServlet.java
@@ -0,0 +1,30 @@
+package Servlet.admin;
+
+import Service.Admin.RewardPendingListService;
+import Service.Login.RoleAuthorizationService;
+import Service.Login.RoleAuthorizationServiceImpl;
+import Servlet.login.ServletEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class RewardTableAAnnualIntervalSettingServlet extends ServletEntryPoint {
+
+
+    private static final String URL = "WEB-INF/jsp/admin/RewardTableAAnnualIntervalSetting.jsp";
+    private RoleAuthorizationService roleAuthorizationService = new RoleAuthorizationServiceImpl();
+    private RewardPendingListService rewardPendingListService = new RewardPendingListService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        req.setAttribute("flag",!roleAuthorizationService.isResearchAndDevelopmentOffice(session));
+        req.setAttribute("pendingReward",rewardPendingListService.havePendingReward((String) session.getAttribute("userRole")));
+        req.setAttribute("returnedPendingReward",rewardPendingListService.haveReturnedPendingReward((String) session.getAttribute("userRole")));
+
+        req.getRequestDispatcher(URL).forward(req, resp);
+    }
+}
